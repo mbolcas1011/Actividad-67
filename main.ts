a599c5d77d7527c94b919c472e2dc075fd3aedf2
mbolcas1011 <132547406+mbolcas1011@users.noreply.github.com>
2023-05-18 09:09:15 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,13 @@
-
+input.onButtonPressed(Button.A, function () {
+    radio.sendNumber(-25)
+})
+input.onButtonPressed(Button.AB, function () {
+    radio.sendString("POWER")
+})
+radio.onReceivedString(function (receivedString) {
+	
+})
+input.onButtonPressed(Button.B, function () {
+    radio.sendNumber(25)
+})
+radio.setGroup(1)
